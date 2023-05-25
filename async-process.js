57ee5b60394e9e2754de22df7a9aff47d2dd7221
rfnp <rizkyfajarnp@gmail.com>
2023-05-25 20:17:16 +0700

    Add Promise in async

--- async-process.js
+++ async-process.js
@@ -1,26 +1,52 @@
-function getUsers(isOffline, callback) {
-    // simulate network delay
-    setTimeout(() => {
-        const users = ['John', 'Jack', 'Abigail'];
+// Callback based code
+// function getUsers(isOffline, callback) {
+//     // simulate network delay
+//     setTimeout(() => {
+//         const users = ['John', 'Jack', 'Abigail'];
 
-        if(isOffline) {
-            callback(new Error('cannot retrieve users due offline'), null);
+//         if(isOffline) {
+//             callback(new Error('cannot retrieve users due offline'), null);
 
-            return;
-        }
+//             return;
+//         }
 
-        callback(null, users)
-    }, 3000)
+//         callback(null, users)
+//     }, 3000)
+// }
+
+// function usersCallback(error, users) {
+//     if(error) {
+//         console.log('process failed:', error.message);
+//         return;
+//     }
+
+//     console.log('process success:', users)
+// }
+
+// getUsers(false, usersCallback); // process success: ['John', 'Jack', 'Abigail']
+// getUsers(true, usersCallback); // process failed: cannot retrieve users due to offline
+
+// Using Promise in async process
+function getUsers(isOffline) {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            const users = ['John', 'Jack', 'Abigail'];
+
+            if(isOffline) {
+                reject(new Error('cannot retrieve users due to offline'));
+
+                return;
+            }
+
+            resolve(users);
+        }, 3000);
+    });
 }
 
-function usersCallback(error, users) {
-    if(error) {
-        console.log('process failed:', error.message);
-        return;
-    }
+getUsers(false)
+.then(users => console.log(users))
+.catch(err => console.log(err.message));
 
-    console.log('process success:', users)
-}
-
-getUsers(false, usersCallback); // process success: ['John', 'Jack', 'Abigail']
-getUsers(true, usersCallback); // process failed: cannot retrieve users due to offline
+getUsers(true)
+.then(users => console.log(users))
+.catch(err => console.log(err.message));
